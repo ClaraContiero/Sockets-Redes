6fca3869c99503ac445a89c8b131924cb1450523
Clara Contiero <contiero.clara6@gmail.com>
2026-03-06 18:21:28 -0300

    Adicionando threads

--- client.py
+++ client.py
@@ -16,9 +16,15 @@
 import socket
 import numpy as np
 import socket
+import threading
 
 
 # --------------- Funções Principais
+def enviar_comando(socket):
+    comando = input('\n--- DIGITE O CÓDIGO DA AÇÃO QUE DESEJA REALIZAR ---\n|1| ACEITAR\n|2| CANCELAR\n|3| MOSTRAR STATUS\n|4| SAIR\n')  # aqui é o que a gente envia o que o motorista quer pro servidor
+    socket.sendall(comando.encode('utf-8')) 
+
+
 
 def motorista(host = 'localhost', port=8082): 
     soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -28,21 +34,29 @@ def motorista(host = 'localhost', port=8082):
     print("Conectando ao endereço %s na porta %s" % conectar_servidor) 
     soquete.connect(conectar_servidor) 
 
-    # acho que aqui é o que a gente envia o que o motorista quer pro servidor
-    
-    message = input('\nDIGITE AQUI O QUE DESEJA FAZER:\n')
-    print("Enviando... %s" % message) 
 
-    soquete.sendall(message.encode('utf-8')) 
+    # Thread 1
+    recebe_evento = soquete.recv(2048)
+    print(f"{recebe_evento.decode('utf-8')}")
+
+
+    # Thread 2
+    
+    thread1_comandos = threading.Thread(
+        target=enviar_comando, 
+        args=(soquete,) # parâmetro da função gerar evento
+    )
+    
+    thread1_comandos.start()
 
     # aqui estamos tentando garantir que os dados cheguem completos ao outro lado
-    amount_received = 0 
-    amount_expected = len(message) 
+    # amount_received = 0 
+    # amount_expected = len(comando) 
 
-    while amount_received < amount_expected: 
-        data = soquete.recv(1024) 
-        amount_received += len(data) 
-        print ("Recebido do Servidor: %s" % data)  
+    # while amount_received < amount_expected: 
+    data = soquete.recv(1024) 
+    # amount_received += len(data) 
+    print (f"Recebido do Servidor: {data.decode('utf-8')}" )  
 
     # e aqui é o comando que encerra o programa
     soquete.close()
@@ -50,4 +64,5 @@ def motorista(host = 'localhost', port=8082):
 
 
 
+# chamando a função motorista
 motorista()
--- server.py
+++ server.py
@@ -15,8 +15,8 @@
 
 import socket
 import numpy as np
-
-
+import threading
+import time
 
 
 # --------------- Variáveis Globais
@@ -26,8 +26,6 @@ status = True # se True, motorista está disponível, se False, não está
 sair = False
 
 
-
-
 # --------------- Funções Principais
 
 def gerar_corrida():
@@ -35,73 +33,73 @@ def gerar_corrida():
   dist_total = np.random.randint(1, 100)
   global valor_total
   valor_total = (dist_total * 2.50) * 0.25
-  notif = "Distância para o Início da Corrida: {dist_inicial} Km\nDistância da Corrida: {dist_total} Km\nValor a Ser Recebido: {round(valor_total,2)} reais"
+  notif = f"\n--- OLÁ, MOTORISTA! ---\nDistância para o Início da Corrida: {dist_inicial} Km\nDistância da Corrida: {dist_total} Km\nValor a Ser Recebido: {round(valor_total,2)} reais\n"
   return notif
 
-   
 def mostrar_status(status):
-   if status == True:
-    print('Disponível')
-   else:
-      print('Ocupado')
+  if status == True:
+    return 'Status: Disponível'
+  else:
+    return 'Status: Ocupado'
+  
+def gerar_evento(client):
+  while True:
+    time.sleep(5)
+    client.sendall(gerar_corrida().encode('utf-8'))
 
 
-# operação local, porta TCP/UDP (local host indica basicamente o IP da nossa máquina)
-def servidor(host='localhost', port=8082): 
-    global status
-    global sair
-    
-    # quantidade máxima de dados que pode ser recebida de uma vez
-    data_payload =  2048 
+def servidor(host='localhost', port=8082): # operação local, porta TCP/UDP (local host indica basicamente o IP da nossa máquina)
 
-    # com o soquete criado, estamos definindo que iremos usar IPv4 e que o protocolo de transporte será o TCP (queremos que os dados cheguem inteiros)
-    soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+  global status
+  global sair
+  
+  data_payload =  2048 # quantidade máxima de dados que pode ser recebida de uma vez
 
-    # é o que vai associar o nosso soquete criado com o endereço IP e a porta que estamos utilizando
-    conectar_servidor = (host, port) 
-    print('Inicializando servidor -> %s port %s' %conectar_servidor)
-    soquete.bind(conectar_servidor)
-
-    soquete.listen(5) # aqui podemos colocar um parâmetro que indica quantas conexões pendentes serão permitidas antes de recusar novas conexões
+  soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # com o soquete criado, estamos definindo que iremos usar IPv4 e que o protocolo de transporte será o TCP (queremos que os dados cheguem inteiros)
 
 
-    while sair != True:
-        print('Esperando mensagem do cliente.')
-        
-        motorista, endereco = soquete.accept()
+  conectar_servidor = (host, port) # é o que vai associar o nosso soquete criado com o endereço IP e a porta que estamos utilizando
 
-        resposta = motorista.recv(data_payload) # aqui é onde ele recebe a mensagem do client
-        resposta = resposta.decode().strip().upper()
+  print('Inicializando servidor -> %s port %s' %conectar_servidor)
+  soquete.bind(conectar_servidor)
+
+  soquete.listen(5) # aqui podemos colocar um parâmetro que indica quantas conexões pendentes serão permitidas antes de recusar novas conexões
 
 
-        if resposta == ('ACEITAR'): # então o status muda pra ocupado
-          status = False
-          print("Data:' %s" %resposta)
-          motorista.sendall("CORRIDA ACEITA".encode('utf-8'))
+  print('Esperando o motorista...')
+  
+  motorista, endereco = soquete.accept()
 
-        elif resposta == ('CANCELAR'): # status muda pra disponível
-          status = True
-          print("Data:' %s" %resposta)
-          motorista.sendall("CORRIDA CANCELADA".encode('utf-8'))
+  # Thread 2
 
-        elif resposta == ('MOSTRAR STATUS'):
-          print("Data:' %s" %resposta)
-          motorista.sendall(f"STATUS: {mostrar_status(status).encode('utf-8')}")
+  thread2_evento = threading.Thread(
+    target=gerar_evento, 
+    args=(motorista,) # parâmetro da função gerar evento
+)
+  
+  thread2_evento.start()
 
-        elif resposta == ('SAIR'):
-          sair = True
-          print('Saindo da aplicação...')
-          break
-          motorista.close()
-        else:
-          print('Comando não identificado') # isso ainda precisa ser tratado..
-          motorista.sendall('ERRO! Comando não identificado'.encode('utf-8'))
-        motorista.close()
+  # Thread 1
+  resposta = motorista.recv(data_payload) # aqui é onde ele recebe a mensagem do client (se vai aceitar, cancelar, sair ou mostrar status)
+  resposta_decode = resposta.decode('utf-8').strip()
 
-servidor()
+  if resposta_decode == ('1'): 
+    status = False # então o status muda pra ocupado
+    motorista.sendall("CORRIDA ACEITA".encode('utf-8'))
+
+  elif resposta_decode == ('2'): 
+    status = True # status muda pra disponível
+    motorista.sendall("CORRIDA CANCELADA".encode('utf-8'))
+
+  elif resposta_decode == ('3'):
+    motorista.sendall(mostrar_status(status).encode('utf-8'))
+
+  elif resposta_decode == ('4'):
+    sair = True
+    print('Saindo da aplicação...')
+    motorista.sendall('Saindo da Aplicação...'.encode('utf-8'))
+    motorista.close()
 
 
 
-
-
-    
+servidor() # chamando a função servidor
