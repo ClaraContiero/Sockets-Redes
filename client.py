b64392831416b12da3359093fa10db7d9b58d61c
Clara Contiero <contiero.clara6@gmail.com>
2026-03-30 17:21:41 -0300

    commit final 3 fase

--- client.py
+++ client.py
@@ -12,17 +12,25 @@ def notifica(socket):
             if not corrida:
                 break
             print(f'\n[NOTIFICAÇÃO RECEBIDA]:\n {corrida.decode('utf-8')}')
-        except:
+        except Exception as e:
+            print('Erro', e)
             break
 
 
 # --------------- Parte 2: Enviar Comandos
 def enviar_comando(socket):
     while True:
-        comando = input()  # aqui é o que a gente envia o que o motorista quer pro servidor
-        socket.sendall(comando.encode('utf-8')) 
+        # ------ esse try é pra pegar erro de comandos inapropriados
+        try:
+            comando = input()  # aqui é o que a gente envia o que o motorista quer pro servidor
+            socket.sendall(comando.encode('utf-8')) 
+        except (EOFError, KeyboardInterrupt):
+            print('\nNão é mais possível enviar comandos.')
+            break
+        except Exception as e:
+            print('\n Problema inesperado no envio ->', e)
+            break
         
-
 
 # --------------- Cliente Completo
 def motorista(host = 'localhost', port=8082): 
@@ -31,24 +39,38 @@ def motorista(host = 'localhost', port=8082):
     soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     conectar_servidor = (host, port) 
     soquete.connect(conectar_servidor) # fazendo a ponte com o server
+    
+    status_conexao = soquete.recv(2048).decode('utf-8')
 
-    # ------ entrando com o nome de usuário e enviando para o server
-    nome =  input('Entre com o seu nome de usuário:\n')
-    soquete.sendall(nome.encode('utf-8'))
+    if status_conexao == 'LIMITE ATINGIDO.':
+        print("\nO servidor está cheio. Tente novamente mais tarde.")
+        soquete.close()
+        return
+    elif status_conexao == 'OK.':
+        # ------ entrando com o nome de usuário e enviando para o server
+        nome =  input('Entre com o seu nome de usuário:\n')
+        soquete.sendall(nome.encode('utf-8'))
+
 
     
-    # ------ thread 1 - notificar eventos de corrida
-    thread1_escuta = threading.Thread(target=notifica, args=(soquete,))
-    thread1_escuta.start()
-  
-    # ------ thread 2 - enviar comandos para o server
-    thread2_envia = threading.Thread(target=enviar_comando, args=(soquete,))
-    thread2_envia.start()
-    thread2_envia.join()
+        # ------ thread 1 - notificar eventos de corrida
+        thread1_escuta = threading.Thread(target=notifica, args=(soquete,))
+        thread1_escuta.start()
+    
+        # ------ thread 2 - enviar comandos para o server
+        thread2_envia = threading.Thread(target=enviar_comando, args=(soquete,))
+        thread2_envia.start()
 
-
-    soquete.close()
-
+        # ------ pra evitar o problema do ctrl-c
+        try: 
+            thread2_envia.join() 
+        except KeyboardInterrupt:
+            print("\nFinalizando aplicação...")
+        finally:
+            soquete.close()
+            print("Conexão encerrada!")
+    
 
 # --------------- Execução do Programa
-motorista() 
+if __name__ == "__main__":
+    motorista()
--- server.py
+++ server.py
@@ -7,7 +7,11 @@ import random
 import json
 
 
-# --------------- Impedindo que haja 'competição' entre as  threads, vamos usar o lock para isso
+# --------------- Para impedir que haja 'competição' entre as  threads, vamos usar o lock para isso
+
+max_conexoes = 2
+conexoes_ativas = 0
+lock_conexoes = threading.Lock()
 lock_dados = threading.Lock()
 
 # --------------- Parte 1: Gerar Eventos de Corrida
@@ -20,9 +24,11 @@ def gerar_evento(client, dados):
             espera = 0 if primeira_corrida else 10
             time.sleep(espera)
             
+            # interrompe o programa se sair = True
             if dados['sair']:
                 break
 
+            # chamando a função de gerar evento e calculando valor atual
             if dados['status'] == True:
                 dist_inicial = np.random.randint(1, 20)
                 dist_total = np.random.randint(1, 100)
@@ -35,9 +41,9 @@ def gerar_evento(client, dados):
                 
                 client.sendall(notif.encode('utf-8'))
                 
-                time.sleep(8)
 
-                # notificação para quando demorar demais pra responder (status true significa que ainda não aceitou)
+                # se passar 8 segundos e cliente ainda não aceitou
+                time.sleep(8)
                 if dados['status'] == True:
                   cancelar = 'CORRIDA CANCELADA'.encode('utf-8')
                   novo_preco = round(dados['valor_atual'] + (dados['valor_atual'] * 0.35), 2)
@@ -55,20 +61,16 @@ def gerar_evento(client, dados):
             else:
                 client.sendall('\n[Finalize a corrida atual para receber novas]'.encode('utf-8'))
                 time.sleep(5)
+                # ou seja, aqui só envia novas corridas se o status for True (disponível)
             
         except (OSError, ConnectionResetError):
             break
 
 # --------------- Parte 2: Receber Comandos
 
-def mostrar_status(status):
-  if status == True:
-    return 'Status: DISPONÍVEL'
-  else:
-    return 'Status: OCUPADO'
-
 def recebe_comando(client, data, dados, dicio_base):
-    
+    global conexoes_ativas
+
     while True:
         try:
             resposta = client.recv(data).decode('utf-8').strip()
@@ -101,21 +103,23 @@ def recebe_comando(client, data, dados, dicio_base):
                 client.sendall('Saindo...'.encode('utf-8'))
                 break
                   
-        except:
+        except Exception as e:
+            print('Erro:', e)
             break
+
 
     client.close()
 
+    # quando ele sair, vamos diminuir as conexões
+    with lock_conexoes:
+        conexoes_ativas -= 1
+
+
 
 # --------------- Servidor Completo
-def servidor(host='localhost', port=8082): # operação local, porta TCP/UDP (local host indica basicamente o IP da nossa máquina)
-    
-    # primeiramente tentamos abrir o arquivo se ele existe, caso não, criamos um dicionário 
-    try:
-        with open("saldos.json", "r") as arquivo:
-            dicio_base = json.load(arquivo)
-    except (FileNotFoundError, json.JSONDecodeError):
-        dicio_base = {}
+def servidor(dicio_base, host='localhost', port=8082): # operação local, porta TCP/UDP (local host indica basicamente o IP da nossa máquina)
+    global conexoes_ativas
+
   
     # ------ conexão com o socket
     carga_dados =  2048 # quantidade máxima de dados que pode ser recebida de uma vez
@@ -127,35 +131,66 @@ def servidor(host='localhost', port=8082): # operação local, porta TCP/UDP (lo
     print('Esperando o motorista...')
  
     while True:
-      motorista, endereco = soquete.accept()
-      nome_motorista = motorista.recv(2048).decode('utf-8')
+        motorista, endereco = soquete.accept()
 
-      # testando se o motorista atual está na lista, caso não, botamos ele lá
-      if nome_motorista not in dicio_base:
-          dicio_base[nome_motorista] = {
-              'valor_atual': 0,
-              'saldo_total': 0,
-              'status': True,
-              'sair': False
-          }
-          # Salva o novo motorista de forma protegida
-          with lock_dados:
-              with open("saldos.json", "w") as arquivo:
-                  json.dump(dicio_base, arquivo, indent=4)
-                  
-
-      dados_motorista = dicio_base[nome_motorista]
+        with lock_conexoes:
+            if conexoes_ativas >= max_conexoes:
+                motorista.sendall('LIMITE ATINGIDO.'.encode('utf-8'))
+                motorista.close()
+                continue
+            else:
+                conexoes_ativas += 1
+                motorista.sendall('OK.'.encode('utf-8'))
+                
 
 
-      # ------ thread 2 - gerar eventos de corrida
-      thread2_evento = threading.Thread(target=gerar_evento, args=(motorista, dados_motorista) ) # parâmetro da função gerar evento
-      thread2_evento.start()
+        nome_motorista = motorista.recv(2048).decode('utf-8')
+
+        # ------ testando se o motorista atual está na lista, caso não, botamos ele lá
+        if nome_motorista not in dicio_base:
+            dicio_base[nome_motorista] = {
+                'valor_atual': 0,
+                'saldo_total': 0,
+                'status': True,
+                'sair': False
+            }
+
+            # ------ salvamos o novo motorista usando o lock para evitar concorrência
+            with lock_dados:
+                with open("saldos.json", "w") as arquivo:
+                    json.dump(dicio_base, arquivo, indent=4)
+                    
+
+        dados_motorista = dicio_base[nome_motorista]
+        dados_motorista['sair'] = False
 
 
-      # ------ thread 1 - receber comando E atualiza a fila de motoristas (À SER FEITO)
-      thread1_recebe = threading.Thread(target=recebe_comando, args=(motorista, carga_dados,dados_motorista, dicio_base ) ) # parâmetro da função gerar evento
-      thread1_recebe.start()
+        # ------ thread 2 - gerar eventos de corrida
+        thread2_evento = threading.Thread(target=gerar_evento, args=(motorista, dados_motorista) ) # parâmetro da função gerar evento
+        thread2_evento.start()
+
+
+        # ------ thread 1 - receber comando E atualiza a fila de motoristas (À SER FEITO)
+        thread1_recebe = threading.Thread(target=recebe_comando, args=(motorista, carga_dados,dados_motorista, dicio_base ) ) # parâmetro da função gerar evento
+        thread1_recebe.start()
 
 
 # --------------- Execução do Programa
-servidor()
+if __name__ == "__main__":
+
+     # ------ primeiramente tentamos abrir o arquivo se ele existe, caso não, criamos um dicionário vazio
+    try:
+        with open("saldos.json", "r") as arquivo:
+            dicio_base = json.load(arquivo)
+    except (FileNotFoundError, json.JSONDecodeError):
+        dicio_base = {}
+
+    # ------ caso dê algum problema, ao menos vai ficar salvo no arquivo
+    try: 
+        servidor(dicio_base)
+    except KeyboardInterrupt:
+        print('Servidor interrompido pelo motorista.')
+        with lock_dados:
+            with open("saldos.json", "w") as f:
+                json.dump(dicio_base, f, indent=4)
+        print("Dados salvos. Encerrando programa...")
